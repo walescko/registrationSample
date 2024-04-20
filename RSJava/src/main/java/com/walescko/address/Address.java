8c09135e5e8d1f59664397cafb57dcf6f12efde9
walescko <walescko@gmail.com>
2024-04-19 22:48:44 -0300

    java - getters and setters

--- RSJava/src/main/java/com/walescko/address/Address.java
+++ RSJava/src/main/java/com/walescko/address/Address.java
@@ -18,4 +18,60 @@ public class Address {
         this.city = city;
         this.state = state;
     }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public int getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(int CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
--- RSJava/src/main/java/com/walescko/people/People.java
+++ RSJava/src/main/java/com/walescko/people/People.java
@@ -20,7 +20,53 @@ public class People {
         this.cpf = cpf;
         this.genere = genere;
         this.birthday = birthday;
+    }
 
-        
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public char getGenere() {
+        return genere;
+    }
+
+    public void setGenere(char genere) {
+        this.genere = genere;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 }
